123870c375c3ad6bf461a673863f444b77fa8e44
rmedina09 <raulmp@ciencias.unam.mx>
2019-05-22 19:41:22 -0500

    Update respaldo_auto.sh

--- respaldo_auto.sh
+++ respaldo_auto.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rsync -avtbr -e 'ssh' --rsync-path='sudo rsync' --files-from='/$PATH_TXT/archivos_a_respaldar.txt' --exclude-from='/$PATH_SCRIPTS/archivos_a_omitir.txt' --delete-excluded --filter='protect <carpeta_respaldo>*' <alias_servidor>:/  /$PATH_BACKUP/<carpeta_respaldo>
+rsync -avtbr -e 'ssh' --rsync-path='sudo rsync' --files-from='/$PATH_TXT/archivos_a_respaldar.txt' --exclude-from='/$PATH_TXT/archivos_a_omitir.txt' --delete-excluded --filter='protect <carpeta_respaldo>*' <alias_servidor>:/  /$PATH_BACKUP/<carpeta_respaldo>
 
 rsync -avtbr --delete-excluded --filter='protect <carpeta_respaldo>*' <alias_servidor>:/$PATH_REMOTE/respaldos/BasesDatos /$PATH_BACKUP/<carpeta_respaldo>
 
