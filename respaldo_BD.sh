0436e399cdf0c7e719418d183fd9920b4a4d1990
rmedina09 <raulmp@ciencias.unam.mx>
2019-05-30 16:36:32 -0500

    Update respaldo_BD.sh

--- respaldo_BD.sh
+++ respaldo_BD.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-pg_dump -v -h localhost -d contingencia -f  /$PATH/BasesDatos/contingencia.sql
-pg_dump -v -h localhost -d datafest     -f  /$PATH/BasesDatos/datafest.sql
-pg_dump -v -h localhost -d db_sms       -f  /$PATH/BasesDatos/db_sms.sql
-pg_dump -v -h localhost -d maincca      -f  /$PATH/BasesDatos/maincca.sql
-pg_dump -v -h localhost -d pumabus      -f  /$PATH/BasesDatos/pumabus.sql
-pg_dump -v -h localhost -d viajandodf   -f  /$PATH/BasesDatos/viajandodf.sql
+pg_dump -v -h localhost -d contingencia -f  /$PATH_REMOTE_DB/contingencia.sql
+pg_dump -v -h localhost -d datafest     -f  /$PATH_REMOTE_DB/datafest.sql
+pg_dump -v -h localhost -d db_sms       -f  /$PATH_REMOTE_DB/db_sms.sql
+pg_dump -v -h localhost -d maincca      -f  /$PATH_REMOTE_DB/maincca.sql
+pg_dump -v -h localhost -d pumabus      -f  /$PATH_REMOTE_DB/pumabus.sql
+pg_dump -v -h localhost -d viajandodf   -f  /$PATH_REMOTE_DB/viajandodf.sql
