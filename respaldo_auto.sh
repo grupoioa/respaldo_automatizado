06243da0851af940c034fe2f214392858ec6314e
rmedina09 <raulmp@ciencias.unam.mx>
2019-06-03 15:13:03 -0500

    Update respaldo_auto.sh

--- respaldo_auto.sh
+++ respaldo_auto.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-rsync -avtbr -e 'ssh' --rsync-path='sudo rsync' --files-from='/$PATH_TXT/archivos_a_respaldar.txt' --exclude-from='/$PATH_TXT/archivos_a_omitir.txt' --delete-excluded --filter='protect <carpeta_respaldo>*' <alias_servidor>:/  /$PATH_BACKUP/<carpeta_respaldo>
+# Rutas
+PATH_REMOTE_DB='/home/raul/respaldos/BasesDatos'
+PATH_NCMWS='/usr/local/owgisconfig/ncwms'
 
-rsync -avtbr --delete-excluded --filter='protect <carpeta_respaldo>*' <alias_servidor>:/$PATH_REMOTE_BD/respaldos/BasesDatos /$PATH_BACKUP/<carpeta_respaldo>
+rsync -avtbr -e 'ssh' --rsync-path='sudo rsync' --files-from='/$PATH_TXT/archivos_a_respaldar.txt' --exclude-from='/$PATH_TXT/archivos_a_omitir.txt' --delete-excluded --filter='protect my_folder_backup>*' my_server:/  /$PATH_BACKUP/my_folder_backup
 
-rsync -avtbr --delete-excluded --min-size=100k --filter='protect respaldo_*' <alias_servidor>:/$PATH_REMOTE/owgisconfig/ncwms/config.xml  /$PATH_BACKUP/<carpeta_respaldo>/<carpeta_owgis_config>
+rsync -avtbr --delete-excluded --filter='protect my_folder_backup*' my_server:/$PATH_REMOTE_DB /$PATH_BACKUP/my_folder_backup
 
-rsync -avtbr --delete-excluded --filter='protect respaldo_*' <alias_servidor>:/$PATH_REMOTE/owgisconfig/ncwms/palettes /$PATH_BACKUP/<carpeta_respaldo>/<carpeta_owgis_config>
+rsync -avtbr --delete-excluded --min-size=100k --filter='protect respaldo_*' my_server:/$PATH_REMOTE/owgisconfig/ncwms/config.xml  /$PATH_BACKUP/my_folder_backup/my_folder_owgis_config
+
+rsync -avtbr --delete-excluded --filter='protect my_folder_backup*' my_server:/usr/local/owgisconfig/ncwms/palettes /$PATH_BACKUP/my_folder_backup/my_folder_owgis_config
