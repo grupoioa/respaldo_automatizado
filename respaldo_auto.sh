846d44d1f3bdb73c0574ce52af9af50b74ea6974
rmedina09 <raulmp@ciencias.unam.mx>
2019-06-03 15:41:55 -0500

    Update respaldo_auto.sh

--- respaldo_auto.sh
+++ respaldo_auto.sh
@@ -10,6 +10,6 @@ rsync -avtbr -e 'ssh' --rsync-path='sudo rsync' --files-from='/$PATH_TXT/archivo
 
 rsync -avtbr --delete-excluded --filter='protect my_folder_backup*' OWGIS_server:/$PATH_REMOTE_DB /$PATH_BACKUP/my_folder_backup
 
-rsync -avtbr --delete-excluded --min-size=100k --filter='protect respaldo_*' OWGIS_server:/$PATH_REMOTE/owgisconfig/ncwms/config.xml  /$PATH_BACKUP/my_folder_backup/my_folder_owgis_config
+rsync -avtbr --delete-excluded --min-size=100k --filter='protect respaldo_*' OWGIS_server:/PATH_NCMWS/config.xml  /$PATH_BACKUP/my_folder_backup/my_folder_owgis_config
 
-rsync -avtbr --delete-excluded --filter='protect my_folder_backup*' OWGIS_server:/usr/local/owgisconfig/ncwms/palettes /$PATH_BACKUP/my_folder_backup/my_folder_owgis_config
+rsync -avtbr --delete-excluded --filter='protect my_folder_backup*' OWGIS_server:/PATH_NCMWS/palettes /$PATH_BACKUP/my_folder_backup/my_folder_owgis_config
